fa0228dda35d7f0ec2c1dfe2dd6f4806c0bbac91
jtnunley <jtnunley01@gmail.com>
2023-03-25 06:20:36 -0700

    stash :-(

--- src/lib.rs
+++ src/lib.rs
@@ -24,10 +24,11 @@
 extern crate alloc;
 
 use alloc::vec;
+use core::cmp;
 
 use slotmap::{new_key_type, SlotMap};
 use smallvec::SmallVec;
-use tinyvec::ArrayVec;
+use tinyvec::{ArrayVec, TinyVec};
 
 /// The key type for windows.
 #[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
@@ -68,12 +69,9 @@ impl WindowTable {
 
     /// Iterate over the windows.
     pub fn iter(&self) -> impl ExactSizeIterator<Item = (WindowKey, Rectangle)> + '_ {
-        self.windows.iter().map(|(key, window)| {
-            (
-                WindowKey(key),
-                window.rect
-            )
-        })
+        self.windows
+            .iter()
+            .map(|(key, window)| (WindowKey(key), window.rect))
     }
 
     /// Empties the window table.
@@ -88,14 +86,16 @@ impl WindowTable {
     }
 
     /// Insert a new window.
-    pub fn insert(&mut self, rect: Rectangle) -> WindowKey {
+    pub fn insert(&mut self, rect: Rectangle) -> Result<WindowKey, InsertError> {
+        use alloc::vec;
+
         let key = {
             let inner = self.windows.insert(Window {
                 rect,
                 parents: SmallVec::new(),
                 children: SmallVec::new(),
             });
-            
+
             WindowKey(inner)
         };
 
@@ -104,7 +104,19 @@ impl WindowTable {
             Some(root) => root,
             None => {
                 self.root = Some(key);
-                return key;
+                return Ok(key);
+            }
+        };
+
+        // We should be able to fit this window in the root window.
+        if rect.intersects(self.windows[root.0].rect) {
+            return Err(InsertError::OutsideRoot);
+        }
+        let root = match self.root {
+            Some(root) => root,
+            None => {
+                self.root = Some(key);
+                return Ok(key);
             }
         };
 
@@ -116,17 +128,40 @@ impl WindowTable {
             self.windows[parent.0].children.push(key);
         }
 
-        // Set parents.
-        self.windows[key.0].parents = parents;
+        // The current rectangles and windows we're testing.
+        let mut current_rects = tinyvec::tiny_vec![[Rectangle; 4] => rect];
+        let mut current_windows: SmallVec<[WindowKey; 3]> = smallvec::smallvec![root];
 
-        key
+        while !current_rects.is_empty() {
+            // Take the rectangles.
+            let mut taken_rects = core::mem::take(&mut current_rects);
+            let mut taken_windows = core::mem::take(&mut current_windows);
+
+            // Drain the rectangles.
+            while !taken_rects.is_empty() {
+                let rect = taken_rects.swap_remove(0);
+
+                // Find the intersection of the rectangle with the windows.
+                for window in &taken_windows {
+                    let window_rect = self.windows[window.0].rect;
+                    let (intersection, remainder) = match rect.intersection(window_rect) {
+                        Some(x) => x,
+                        None => continue,
+                    };
+
+                    // This window's children should be carried over to the next iteration.
+                }
+            }
+        }
+
+        Ok(key)
     }
 
     /// Get the windows that this rectangle intersects, at this root.
     fn intersections(
         &self,
         roots: &[WindowKey],
-        rect: Rectangle
+        rect: Rectangle,
     ) -> (SmallVec<[WindowKey; 3]>, bool) {
         let mut windows = SmallVec::new();
         let mut rectangles = tinyvec::tiny_vec![[Rectangle; 4] => rect];
@@ -142,10 +177,8 @@ impl WindowTable {
 
             if let Some((intersection, remainder, matched_root)) = intersect {
                 // Re-run this code on the new root's children.
-                let (mut child_intersections, leftovers) = self.intersections(
-                    &self.windows[matched_root.0].children,
-                    intersection
-                );
+                let (mut child_intersections, leftovers) =
+                    self.intersections(&self.windows[matched_root.0].children, intersection);
 
                 // Append the results to our list.
                 windows.append(&mut child_intersections);
@@ -169,6 +202,13 @@ impl WindowTable {
 
         (windows, leftovers)
     }
+}
+
+/// Error with inserting a window.
+#[derive(Debug)]
+pub enum InsertError {
+    /// This window falls outside of the bounds of the root window.
+    OutsideRoot,
 }
 
 /// The current cursor state.
@@ -218,12 +258,90 @@ impl Rectangle {
         (self.bottom - self.top).abs()
     }
 
+    fn area(&self) -> i32 {
+        self.width() * self.height()
+    }
+
+    /// Tell if two windows intersect.
+    fn intersects(&self, other: Self) -> bool {
+        self.left < other.right
+            && self.right > other.left
+            && self.top < other.bottom
+            && self.bottom > other.top
+    }
+
     /// Intersect this rectangle with another.
-    fn intersection(
-        mut self,
-        other: Self
-    ) -> Option<(Self, ArrayVec<[Self; 4]>)> {
-        todo!() 
+    ///
+    /// Returns the intersection and the remainder of the `Self` rectangle.
+    fn intersection(mut self, other: Self) -> Option<(Self, ArrayVec<[Self; 4]>)> {
+        // See if the rectangles intersect.
+        if self.intersects(other) {
+            return None;
+        }
+
+        let mut remainder = ArrayVec::new();
+
+        // See if the bottom edge of this rectangle intersects the top edge of the other.
+        if self.bottom > other.top {
+            let new_top = cmp::max(self.top, other.top);
+
+            // Push the remainder of the top edge if there is any.
+            if new_top != self.top {
+                remainder.push(Rectangle {
+                    bottom: new_top,
+                    ..self
+                });
+            }
+
+            self.top = new_top;
+        }
+
+        // See if the top edge of this rectangle intersects the bottom edge of the other.
+        if self.top < other.bottom {
+            let new_bottom = cmp::min(self.bottom, other.bottom);
+
+            // Push the remainder of the bottom edge if there is any.
+            if new_bottom != self.bottom {
+                remainder.push(Rectangle {
+                    top: new_bottom,
+                    ..self
+                });
+            }
+
+            self.bottom = new_bottom;
+        }
+
+        // See if the left edge of this rectangle intersects the right edge of the other.
+        if self.left < other.right {
+            let new_right = cmp::min(self.right, other.right);
+
+            // Push the remainder of the right edge if there is any.
+            if new_right != self.right {
+                remainder.push(Rectangle {
+                    left: new_right,
+                    ..self
+                });
+            }
+
+            self.right = new_right;
+        }
+
+        // See if the right edge of this rectangle intersects the left edge of the other.
+        if self.right > other.left {
+            let new_left = cmp::max(self.left, other.left);
+
+            // Push the remainder of the left edge if there is any.
+            if new_left != self.left {
+                remainder.push(Rectangle {
+                    right: new_left,
+                    ..self
+                });
+            }
+
+            self.left = new_left;
+        }
+
+        Some((self, remainder))
     }
 }
 
@@ -232,14 +350,32 @@ mod tests {
     use super::{Rectangle, WindowTable};
 
     #[test]
-    fn intersect() {
+    fn no_intersect() {
+        let a = Rectangle::new(0, 0, 10, 10);
+        let b = Rectangle::new(15, 15, 25, 25);
+
+        assert!(a.intersection(b).is_none());
+    }
+
+    #[test]
+    fn intersect_inside() {
+        let a = Rectangle::new(5, 5, 10, 10);
+        let b = Rectangle::new(0, 0, 15, 15);
+
+        let (intersection, remainder) = a.intersection(b).unwrap();
+        assert_eq!(intersection, a);
+        assert!(remainder.is_empty());
+    }
+
+    #[test]
+    fn intersect_corner() {
         let a = Rectangle::new(0, 0, 10, 10);
         let b = Rectangle::new(5, 5, 15, 15);
 
-        let (a, b) = a.intersection(b).unwrap();
+        let (intersection, remainder) = a.intersection(b).unwrap();
 
-        assert_eq!(a, Rectangle::new(5, 5, 10, 10));
-        assert_eq!(b.as_slice(), &[Rectangle::new(10, 5, 15, 15)]);
+        assert_eq!(intersection, Rectangle::new(5, 5, 10, 10));
+        assert_eq!(remainder.len(), 2);
     }
 
     #[test]
@@ -247,9 +383,9 @@ mod tests {
         let mut window_table = WindowTable::new();
 
         // Inserting a single rectangle should install it as the root.
-        let rect1 = window_table.insert(Rectangle::new(0, 0, 100, 100));
+        let rect1 = window_table.insert(Rectangle::new(0, 0, 100, 100)).unwrap();
         assert_eq!(Some(rect1), window_table.root);
-        
+
         {
             let slot = &window_table.windows[rect1.0];
             assert!(slot.children.is_empty());
